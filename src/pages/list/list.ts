a2af82c071f01352327727d2400bb684190d58a7
HuiG33 <joni.tefke@metropolia.fi>
2018-02-26 15:26:05 +0200

    message

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
 import {HttpErrorResponse} from '@angular/common/http';
 import {LoginPage} from '../login/login';
 import {MediaProvider} from '../../providers/media/media';
 
 @Component({
   selector: 'page-list',
-  templateUrl: 'list.html'
+  templateUrl: 'list.html',
 })
 export class ListPage {
   selectedItem: any;
   filesArray: any;
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items: Array<{ title: string, note: string, icon: string }>;
+
+  searchQuery: string = '';
+  searchItems: any;
 
   doRefresh(refresher) {
     console.log('Begin async operation', refresher);
@@ -23,23 +26,34 @@ export class ListPage {
     }, 2000);
   }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private mediaProvider: MediaProvider) {
+  constructor(
+    public navCtrl: NavController, public navParams: NavParams,
+    private mediaProvider: MediaProvider) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
     // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
 
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+    this.initializeItems();
+  }
+
+  getItems(ev: any) {
+
+    this.initializeItems();
+
+    let val = ev.target.value;
+
+    if (val && val.trim() != '') {
+      this.searchItems = this.searchItems.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
       });
     }
   }
+
+  initializeItems() {
+    this.searchItems;
+  };
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
     if (localStorage.getItem('token') != null) {
@@ -48,11 +62,19 @@ export class ListPage {
         this.mediaProvider.getNewFiles().subscribe(response => {
           console.log(response);
           this.filesArray = response;
+
+          console.log('Title list:');
+          this.searchItems = [];
+          this.filesArray.forEach(file => {
+            this.searchItems.push(file.title);
+            console.log(file.title);
+          });
+
         });
       }, (error: HttpErrorResponse) => {
         console.log(error);
         //this.router.navigate(['login']);
-        this.navCtrl.setRoot(LoginPage)
+        this.navCtrl.setRoot(LoginPage);
       });
     } else {
       //this.router.navigate(['login']);
@@ -63,7 +85,7 @@ export class ListPage {
   itemTapped(event, item) {
     // That's right, we're pushing to ourselves!
     this.navCtrl.push(ListPage, {
-      item: item
+      item: item,
     });
   }
 }
